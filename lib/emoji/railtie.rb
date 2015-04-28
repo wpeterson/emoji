805b082494fe3ae152072307293191f643394ae5
Łukasz Niemier <lukasz@niemier.pl>
2015-04-28 15:53:27 +0200

    Cleanup style a little

--- lib/emoji/railtie.rb
+++ lib/emoji/railtie.rb
@@ -4,7 +4,7 @@ require 'rails'
 module Emoji
   class Railtie < Rails::Railtie
     initializer "emoji.defaults" do
-      Emoji.asset_host = ActionController::Base.asset_host
+      Emoji.asset_host = ActionController::Base.asset_host || ''
       Emoji.asset_path = '/assets/emoji'
       Emoji.use_plaintext_alt_tags = false
     end
