d88b37ebd3ebd6437da106c9c3f5b314030812f1
Kristian Kvarnsbacke <kristian@kmja.com>
2016-06-20 10:31:47 +0200

    Use rails asset prefix as default asset_path configuration

--- lib/emoji/railtie.rb
+++ lib/emoji/railtie.rb
@@ -5,7 +5,7 @@ module Emoji
   class Railtie < Rails::Railtie
     initializer "emoji.defaults" do
       Emoji.asset_host = ActionController::Base.asset_host
-      Emoji.asset_path = '/assets/emoji'
+      Emoji.asset_path = "#{config.assets.prefix}/emoji"
       Emoji.use_plaintext_alt_tags = false
     end
 
